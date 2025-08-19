81e674701c29aa586ab3ed847f6fbca7a8299b90
Ryan McCusker <rjmccusker229@gmail.com>
2025-08-19 13:19:49 -0400

    get events in tile view

--- backend/events/views.py
+++ backend/events/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+from tiling import tile_to_polygon
+from events.models import Event
 
-# Create your views here.
+def get_tile_mvt(request, xtile, ytile, zoom):
+    bbox_polygon = tile_to_polygon(xtile, ytile, zoom)
+    
+    events_in_tile = Event.objects.filter(
+        location__within=bbox_polygon
+    ).order_by("-views")
+
+    
